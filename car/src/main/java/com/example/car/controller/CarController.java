7d0c619e14961100087aa9a8d1c22f51be572f50
Thiago Mattos <euthiagomattos@gmail.com>
2023-07-14 14:18:59 -0300

    changing the api name to cars

--- car/src/main/java/com/example/car/controller/CarController.java
+++ car/src/main/java/com/example/car/controller/CarController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/cars")
 public class CarController {
 
     @Autowired
